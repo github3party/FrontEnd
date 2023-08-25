b90324a39c0e8f22129f0c3e65eff2760ca0b7d8
J0onYEong <cjy2134@naver.com>
2023-08-25 21:29:23 +0900

    [Init] first setting

---
+++ HackerTon0825/HackerTon0825/HackerTon0825App.swift
@@ -0,0 +1,17 @@
+//
+//  HackerTon0825App.swift
+//  HackerTon0825
+//
+//  Created by 최준영 on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HackerTon0825App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
